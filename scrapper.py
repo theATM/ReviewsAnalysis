98e3f09a5e48d918984fc5e6e637c638bdaedce7
Olek <olek@Elios>
2022-10-19 02:30:45 +0300

    Hopefully translation and data scrapping works now

--- scrapper.py
+++ scrapper.py
@@ -4,19 +4,21 @@ from google_play_scraper import Sort, reviews_all
 
 import pandas as pd
 import numpy as np
+import time  # I shall import time this instance!
 
 
-def google_scrapp(app_id,country):
+def google_scrapp(app_id, language, country, amount):
     google_reviews = reviews_all(
         str(app_id),
-        sleep_milliseconds=0,  # defaults to 0
-        lang='en',  # defaults to 'en'
+        sleep_milliseconds=10,  # defaults to 0
+        lang=str(language),  # defaults to 'en'
         country=str(country),  # defaults to 'us'
         sort=Sort.NEWEST,  # defaults to Sort.MOST_RELEVANT
     )
     google_dataframe = pd.DataFrame(np.array(google_reviews), columns=['review'])
     google_dataframe = google_dataframe.join(pd.DataFrame(google_dataframe.pop('review').tolist()))
     google_dataframe.drop(['userImage', 'repliedAt', 'replyContent'], axis=1, inplace=True)
+
 
     return google_dataframe
 
@@ -31,38 +33,21 @@ def apple_scrapp(app_name, app_id, country, how_many):
     return appStore_dataframe
 
 
-class Translator:
-    def __init__(self,model_type='opus-mt',arg_device='cuda',cache_folder="cache/transformers"):
-        from easynmt import EasyNMT
-        import os
-        from torch import cuda, device
-        if arg_device == 'cuda':
-            cuda.empty_cache()
-        self.device = device(arg_device)  # change to cpu if you do not have a gpu else cuda
-        self.model_fi = EasyNMT('opus-mt', cache_folder=cache_folder, device=self.device)
-        #self.model_sw = EasyNMT('mbart50_m2en', cache_folder=cache_folder, device=self.device)
-
-    def translate_fi(self,scrapped_data):
-        scrapped_data['translatedContent'] = scrapped_data['content'].apply(lambda x: self.model_fi.translate(x, target_lang="en", source_lang="fi", max_length=512))
-        return scrapped_data
-
-    def deactivate(self):
-        from torch.cuda import empty_cache
-        if self.device == 'cuda':
-            empty_cache()
-        del self.model_fi
-        del self.model_sw
-
-
 class GoogleTranslator:
     def __init__(self):
         from deep_translator import GoogleTranslator
         self.translator = GoogleTranslator()
 
-    def translate(self, scrapped_data):
-        scrapped_data['translatedContent'] = scrapped_data['content'].apply(
+    def translate(self, data):
+        data['content'] = self.__remove_quotas(data).apply(
             lambda x: self.translator.translate(x, source='auto', target_lang="en"))
-        return scrapped_data
+        return data
+
+    @staticmethod
+    def __remove_quotas(data):
+        import re
+        return data['content'].apply( lambda x:  re.sub('"','',str(x)) )
+
 
 
 
